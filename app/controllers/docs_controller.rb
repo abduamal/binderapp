a3335b95d7319a85ba9ad528ef924049960ad04f
abduamal <abduamal@ymail.com>
2021-01-22 02:26:52 -0600

    set new and create actions

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -7,9 +7,30 @@ class DocsController < ApplicationController
     end
 
     def new
+        @doc = Doc.new
     end
 
     def create
+        # create a new instance of a Doc using params;
+            # title and content in this case
+        # params = Parameters Method
+        # ApplicationController -> ActionController:: -> Parameters Method
+        # params refers to paramerters being passed to the controller via:
+            # GET and POST requests
+                # GET request is passed to the controller via URL from the user's browser
+                    #(ie) .find(params)
+                # POST request is passed via the form, usually
+                    #(ie) .new(params)
+        @doc = Doc.new(doc_params)
+        # redirect vs. render
+        # redirect is a HTTP refresh and all content on the page is lost
+        # render does not refresh the page
+        if @doc.save
+            redirect_to @doc
+        else
+            render 'new'
+        end
+        
     end
 
     def edit 
@@ -27,6 +48,7 @@ class DocsController < ApplicationController
     end
 
     def doc_params
+        params.require(:doc).permit(:title, :content)
     end
     
 end
